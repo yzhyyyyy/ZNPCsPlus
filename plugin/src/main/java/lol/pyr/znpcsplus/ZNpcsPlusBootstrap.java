cefede568b643fc045bd071379b618b8139c73b3
Pyrbu <pyrmcserver@gmail.com>
2024-05-17 05:07:11 +0200

    remove library loader & jar relocator

--- plugin/src/main/java/lol/pyr/znpcsplus/ZNpcsPlusBootstrap.java
+++ plugin/src/main/java/lol/pyr/znpcsplus/ZNpcsPlusBootstrap.java
@@ -31,57 +31,6 @@ public class ZNpcsPlusBootstrap extends JavaPlugin {
             Bukkit.getPluginManager().disablePlugin(this);
             return;
         }
-        /*
-        getLogger().info("Downloading and loading libraries, this might take a while if this is the first time you're launching the plugin");
-        LibraryLoader loader = new LibraryLoader(this, new File(getDataFolder(), "libraries"));
-
-        loader.addRelocation(decrypt("org..bstats"), "lol.pyr.znpcsplus.libraries.bstats");
-        loader.addRelocation(decrypt("me..robertlit..spigotresources"), "lol.pyr.znpcsplus.libraries.spigotresources");
-        loader.addRelocation(decrypt("net..kyori"), "lol.pyr.znpcsplus.libraries.kyori");
-        loader.addRelocation(decrypt("org..checkerframework"), "lol.pyr.znpcsplus.libraries.checkerframework");
-        loader.addRelocation(decrypt("com..google"), "lol.pyr.znpcsplus.libraries.google");
-        loader.addRelocation(decrypt("com..github..retrooper..packetevents"), "lol.pyr.znpcsplus.libraries.packetevents.api");
-        loader.addRelocation(decrypt("io..github..retrooper..packetevents"), "lol.pyr.znpcsplus.libraries.packetevents.impl");
-        loader.addRelocation(decrypt("org..yaml..snakeyaml"), "lol.pyr.znpcsplus.libraries.snakeyaml");
-        loader.addRelocation(decrypt("space..arim..dazzleconf"), "lol.pyr.znpcsplus.libraries.dazzleconf");
-        loader.addRelocation(decrypt("lol..pyr..director"), "lol.pyr.znpcsplus.libraries.command");
-
-        loader.loadLibrary(decrypt("com..google..guava"), "guava", "18.0");
-        loader.loadLibrary(decrypt("com..google..code..gson"), "gson", "2.10.1");
-
-        loader.loadLibrary(decrypt("org..bstats"), "bstats-base", "3.0.2");
-        loader.loadLibrary(decrypt("org..bstats"), "bstats-bukkit", "3.0.2");
-
-        loader.loadLibrary("me.robertlit", "SpigotResourcesAPI", "2.0", "https://repo.pyr.lol/releases");
-
-        loader.loadLibrary(decrypt("com..github..retrooper..packetevents"), "api", "2.3.0", "https://repo.codemc.io/repository/maven-releases/");
-        loader.loadLibrary(decrypt("com..github..retrooper..packetevents"), "spigot", "2.3.0", "https://repo.codemc.io/repository/maven-releases/");
-
-        loader.loadLibrary(decrypt("space..arim..dazzleconf"), "dazzleconf-core", "1.2.1");
-        loader.loadLibrary(decrypt("space..arim..dazzleconf"), "dazzleconf-ext-snakeyaml", "1.2.1");
-        loader.loadLibrary("org.yaml", "snakeyaml", "1.33");
-
-        loader.loadLibrary("lol.pyr", "director-adventure", "2.1.1", "https://repo.pyr.lol/releases");
-
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-api", "4.15.0");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-key", "4.15.0");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-nbt", "4.15.0");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-platform-facet", "4.3.2");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-platform-api", "4.3.2");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-platform-bukkit", "4.3.2");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-text-minimessage", "4.15.0");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-text-serializer-bungeecord", "4.3.2");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-text-serializer-gson", "4.15.0");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-text-serializer-gson-legacy-impl", "4.15.0");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-text-serializer-json", "4.15.0");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-text-serializer-json-legacy-impl", "4.15.0");
-        loader.loadLibrary(decrypt("net..kyori"), "adventure-text-serializer-legacy", "4.15.0");
-        loader.loadLibrary(decrypt("net..kyori"), "examination-api", "1.3.0");
-        loader.loadLibrary(decrypt("net..kyori"), "examination-string", "1.3.0");
-        loader.deleteUnloadedLibraries();
-
-        getLogger().info("Loaded " + loader.loadedLibraryCount() + " libraries!");
-         */
         zNpcsPlus = new ZNpcsPlus(this);
     }
 
@@ -120,10 +69,5 @@ public class ZNpcsPlusBootstrap extends JavaPlugin {
 
     public boolean movedLegacy() {
         return legacy;
-    }
-
-    // Ugly hack because of https://github.com/johnrengelman/shadow/issues/232
-    private static String decrypt(String packageName) {
-        return packageName.replace("..", ".");
     }
 }
