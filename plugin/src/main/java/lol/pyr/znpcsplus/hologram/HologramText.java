4d54dd851288bde605ed87868e62f3263f7065c6
D3v1s0m <d3v1s0m@gmail.com>
2024-06-11 21:31:00 +0530

    %blank% for empty hologram line

--- plugin/src/main/java/lol/pyr/znpcsplus/hologram/HologramText.java
+++ plugin/src/main/java/lol/pyr/znpcsplus/hologram/HologramText.java
@@ -6,13 +6,23 @@ import lol.pyr.znpcsplus.entity.EntityPropertyRegistryImpl;
 import lol.pyr.znpcsplus.packets.PacketFactory;
 import lol.pyr.znpcsplus.util.NpcLocation;
 import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
 
 public class HologramText extends HologramLine<Component> {
+
+    private static final Component BLANK  = Component.text("%blank%");
 
     public HologramText(EntityPropertyRegistryImpl propertyRegistry, PacketFactory packetFactory, NpcLocation location, Component text) {
         super(text, packetFactory, EntityTypes.ARMOR_STAND, location);
         addProperty(propertyRegistry.getByName("name"));
         addProperty(propertyRegistry.getByName("invisible"));
+    }
+
+    @Override
+    public void show(Player player) {
+        if (!getValue().equals(BLANK)) {
+            super.show(player);
+        }
     }
 
     @SuppressWarnings("unchecked")
